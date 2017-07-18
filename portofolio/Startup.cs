c654a9cdd286c349d793e1bc85af059ac660da7d
Joana Godinho <joana.mg0@hotmail.com>
2017-07-18 17:42:13 +0100

    Add project files.

---
+++ portofolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(portofolio.Startup))]
+namespace portofolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
